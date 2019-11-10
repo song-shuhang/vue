b3af4869745464971c837c354bfb4ddc88eb0629
songshuhang <1186444258@qq.com>
2019-11-10 15:39:26 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
       <router-view></router-view>
+      <FooterGuide></FooterGuide>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-  
+  import FooterGuide from './components/FooterGuide/FooterGuide'  
   export default {
+    components:{
+      FooterGuide,
+    },
+    template:'<FooterGuide/>'
+    
   }
 </script>
 
